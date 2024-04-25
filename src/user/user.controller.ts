f307d348e60f34243b48326da48579d60e488df4
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-04-25 22:36:59 +0700

    feat findAll

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,33 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUserDto';
 import { UserService } from './user.service';
 import { CommentService } from './../comment/comment.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ResponseMessage } from 'src/decorator/customize';
+import { CREATE_USER, GET_USER_DETAIL, GET_USERS } from 'src/utils/message';
+import { UserFilterDto } from './dto/filter-user.dto';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService, private readonly commentService: CommentService) { }
 
 
-    @Get(':id')
-    findOne(@Param('id') id: number) {
-        return this.userService.findOne(id);
-    }
     @Post()
+    @ResponseMessage(CREATE_USER)
     create(@Body() createUserDto: CreateUserDto) {
         return this.userService.createUser(createUserDto);
     }
+    @Get(':id')
+    @ResponseMessage(GET_USER_DETAIL)
+    findOne(@Param('id') id: number) {
+        return this.userService.findOne(id);
+    }
+    @Get()
+    @ResponseMessage(GET_USERS)
+    findAll(@Query() query: UserFilterDto) {
+        return this.userService.findAll(query);
+    }
+
     @Get(':id/comments')
     getUserComments(@Param('id') id: string) {
         return this.commentService.findUsersComments(id)
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,24 +1,107 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/createUserDto';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { CONFLICT_EMAIL, CONFLICT_USERNAME, NOTFOUND_USER } from 'src/utils/message';
+import { UserFilterDto } from './dto/filter-user.dto';
 
 @Injectable()
 export class UserService {
 
     constructor(@InjectRepository(User) private readonly userRepo: Repository<User>) { }
 
-    async createUser(createUserDto: CreateUserDto){
-        return await this.userRepo.save(createUserDto);
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        const { email } = createUserDto;
+        const userExist = await this.userRepo.findOneBy({ email });
+        if (userExist) throw new BadRequestException(CONFLICT_EMAIL);
+        const user = this.userRepo.create(createUserDto);
+        return await this.userRepo.save(user);
     }
 
-    async findOne(id: number){
-        return await this.userRepo.findOne({ where: { id: id } });
+    async findOne(id: number) {
+        const user: User = await this.userRepo.findOneBy({ id });
+        if (!user) throw new BadRequestException(NOTFOUND_USER);
+        return user;
+    }
+
+    async findAll(queryObj: UserFilterDto) {
+        const {
+            username,
+            email,
+            phone,
+            isActive,
+            roleId,
+            sortBy,
+            sortDescending,
+            pageSize,
+            current,
+        } = queryObj;
+
+        const defaultLimit = pageSize ? pageSize : 10;
+        const defaultPage = current ? current : 1;
+        const offset = (defaultPage - 1) * defaultLimit;
+
+        const query = this.userRepo
+            .createQueryBuilder('user')
+
+        if (username)
+            query.andWhere('user.username ILike :username', {
+                username: `%${username}%`,
+            });
+        if (email)
+            query.andWhere('user.email ILike :email', {
+                email: `%${email}%`,
+            });
+        if (phone)
+            query.andWhere('user.phone ILike :phone', {
+                phone: `%${phone}%`,
+            });
+        if (typeof isActive === 'boolean')
+            query.andWhere('user.isActive = :isActive', { isActive });
+
+        const totalItems = (await query.getMany()).length;
+        const totalPages = Math.ceil(totalItems / defaultLimit);
+
+        const sortableCriterias = ['username', 'email', 'dob'];
+
+        const result = await query
+            .orderBy(
+                sortableCriterias.includes(sortBy) ? `user.${sortBy}` : '',
+                sortDescending ? 'DESC' : 'ASC',
+            )
+            .offset(offset)
+            .limit(defaultLimit)
+            .getMany();
+
+        const data = result.map((user) => {
+            return {
+                ...user,
+            };
+        });
+
+        return {
+            currentPage: defaultPage,
+            totalPages: totalPages,
+            pageSize: defaultLimit,
+            totalItems: totalItems,
+            items: data,
+        };
     }
 
     async updateUser(id: number, updateUserDto: UpdateUserDto) {
-        return await this.userRepo.update(id, updateUserDto);
+        const { username, password, ...rest } = updateUserDto;
+        const user = await this.userRepo.findOneBy({ username });
+        if (user && user.id !== id)
+            throw new BadRequestException(CONFLICT_USERNAME);
+        const existUser = await this.userRepo.findOne({ where: { id } });
+        if (!existUser) throw new BadRequestException(NOTFOUND_USER);
+
+        return this.userRepo.update(id, {
+            username,
+            password,
+            ...rest,
+        });
     }
 }
